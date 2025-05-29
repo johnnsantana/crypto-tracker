df01be8b4a7bfd8c0c6e425203329f4e59e8801d
Jhônatan Santana <jhonatansantanati@gmail.com>
2025-05-29 00:39:46 -0300

    feat: network layer utility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,16 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/v2/\"")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/v2/\"")
         }
     }
     compileOptions {
@@ -57,9 +61,14 @@ dependencies {
     implementation(libs.bundles.ktor)
 
     testImplementation(libs.junit)
+    testImplementation ("io.mockk:mockk-android:1.14.2")
+    testImplementation ("io.mockk:mockk-agent:1.14.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    testImplementation(kotlin("test"))
 }
--- app/src/main/java/com/johnnsantana/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/johnnsantana/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.johnnsantana.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
@@ -51,7 +50,7 @@ fun CoinListScreen(
 
 @PreviewLightDark
 @Composable
-private fun CoinListScreen() {
+private fun CoinListScreenPreview() {
     CryptoTrackerTheme {
         CoinListScreen(
             state = CoinListState(
